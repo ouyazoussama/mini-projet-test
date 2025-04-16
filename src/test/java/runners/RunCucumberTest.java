04569fc7d527f6d45d3e7b3367a9fcd19c518e04
OUYAZ <oussama.ouyaz@capgemini.com>
2025-04-16 14:27:26 +0100

    new project

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -5,26 +5,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"stepDefinitions","setup" },
-        plugin = {
-                "pretty",
-                "json:target/cucumber-report.json",       // Pour qTest
-                "junit:target/cucumber-report.xml",       // (optionnel) format JUnit
-                "html:target/cucumber-html-report.html"   // Pour consultation locale
-        },
-        tags = "@register or @breakStock or @validQuantity or @mailInvalid or @validSingIn or @register1 or @delete or @AddProduct"
+        glue = {"stepDefinitions","setup" })
 
-        //tags =
-                //"@register"
-               //"@breakStock"
-                //"@validQuantity"
-                //"@mailInvalid"
-                //"@validSingIn"
-                 //"@register1"
-                //"@delete"
-                //"@AddProduct"
-
-)
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
 
